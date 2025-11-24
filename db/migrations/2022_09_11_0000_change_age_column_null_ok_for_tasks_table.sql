057fb85bfedabf55249b52657b63e8825ece55ec
tomoe-martop <tomoe@martop.jp>
2025-11-24 13:50:22 +0900

    dbスキーマはjusetsu-dashbord-resourceリポジトリで管理

--- db/migrations/2022_09_11_0000_change_age_column_null_ok_for_tasks_table.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `task_houses` CHANGE COLUMN `age` `age` smallint unsigned NULL COMMENT '年齢';
-ALTER TABLE `task_houses` CHANGE COLUMN `sex` `sex` smallint unsigned NULL COMMENT '1:man, 2:woman';
-ALTER TABLE `task_houses` CHANGE COLUMN `education` `education` varchar(100) NULL COMMENT '教育歴';
