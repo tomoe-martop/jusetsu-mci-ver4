fdeb15afb19b3b6555037416475224b85cf932bb
tomoe-martop <tomoe@martop.jp>
2026-05-22 10:10:24 +0900

    EOL対応時にSTGに投入すること: 顧客提供パッチ適用 (pred_mci.py)
    - 日次データ充足チェックのロジック修正（OK日が25日未満ならエラー）
    - 日時パース形式を "%Y/%m/%d %H:%M:%S" から "%Y/%m/%d %H:%M" に変更

--- api/pred_mci.py
+++ api/pred_mci.py
@@ -204,20 +204,22 @@ class Predictor:
             )
 
         # Check the number of days with sufficient electric data
-        threshold = int(self.N_MINUTES_PER_DAY * (1 - self.THRESHOLD_ELECTRIC_DATA)) # limit of lack rows per day
+        threshold = int(self.N_MINUTES_PER_DAY * (1 - self.THRESHOLD_ELECTRIC_DATA))  # max allowed NaN per day (72)
         daily_n_nan = np.sum(
-            np.isnan(df.air_conditioner.values.reshape(self.N_DAY_ELECTRIC_DATA, self.N_MINUTES_PER_DAY)), 
+            np.isnan(df.air_conditioner.values.reshape(self.N_DAY_ELECTRIC_DATA, self.N_MINUTES_PER_DAY)),
             axis=1
-        ) # daily number of lack rows
-        if (n_over_threshold_per_day := np.sum(daily_n_nan > threshold)) > self.N_DAY_LIMIT_ELECTRIC_DATA:
+        )  # daily number of NaN rows
+        n_sufficient_days = int(np.sum(daily_n_nan <= threshold))  # days with >=95% data
+        if n_sufficient_days < self.N_DAY_LIMIT_ELECTRIC_DATA:
             raise InvalidInputError(
                 202,
-                f"Electric rate >= 95% is only {n_over_threshold_per_day} days, expected >= 25 days"
+                f"Days with electric rate >= 95% is only {n_sufficient_days} days, "
+                f"expected >= {self.N_DAY_LIMIT_ELECTRIC_DATA} days"
             )
 
         # encode datetime features
         df.date_time_jst = df.date_time_jst.apply(
-            lambda x: datetime.datetime.strptime(x, "%Y/%m/%d %H:%M:%S")
+            lambda x: datetime.datetime.strptime(x, "%Y/%m/%d %H:%M") # :%S
         )
         array_datetime = np.sum(np.array(list(map(self._datetime_encode, df.date_time_jst))), axis=0)
 
